b621319579628919e835541eb8011805155ab4ea
KimJungMin <jungmini0601@gmail.com>
2023-03-27 10:47:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST=7
 
 markdown_text = """
+좋은 엔지니어가 되기 위해 노력하는 김정민입니다! 😁
+
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=jungmini0601&show_icons=true&theme=radical)<br>
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=kJungmin)](https://solved.ac/kJungmin/)
 💡최근 블로그 글
 """ # list of blog posts will be appended here
 

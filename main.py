96998caf44a19510a42dd337f773e1ffe0d15e34
KimJungMin <jungmini0601@gmail.com>
2023-03-27 10:52:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-import feedparser, time
+import feedparser
 
-URL="[https://jungmini-laboratory.tistory.com/rss]"
-RSS_FEED = feedparser.parse(URL)
-MAX_POST=7
+somjang_blog_rss_url = "https://jungmini-laboratory.tistory.com/rss"
+rss_feed = feedparser.parse(somjang_blog_rss_url)
 
+MAX_POST_NUM = 10
+
+latest_blog_post_list = ""
+
+MAX_POST_NUM = 10
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed']
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+    
 markdown_text = """
 좋은 엔지니어가 되기 위해 노력하는 김정민입니다! 😁
 
 ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=jungmini0601&show_icons=true&theme=radical)<br>
-[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=kJungmin)](https://solved.ac/kJungmin/)<br>
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=kJungmin)](https://solved.ac/kJungmin/)<br><br>
 💡최근 블로그 글<br>
-""" # list of blog posts will be appended here
+"""
 
+readme_text = f"{markdown_text}{latest_blog_post_list}"
 
-for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
-        break
-    else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-
-f = open("README.md",mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)

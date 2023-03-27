c9010624d4e1fb04eeecf9bfd5ef166b82395703
KimJungMin <jungmini0601@gmail.com>
2023-03-27 10:49:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ markdown_text = """
 
 ![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=jungmini0601&show_icons=true&theme=radical)<br>
 [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=kJungmin)](https://solved.ac/kJungmin/)<br>
-💡최근 블로그 글
+💡최근 블로그 글<br>
 """ # list of blog posts will be appended here
 
 
